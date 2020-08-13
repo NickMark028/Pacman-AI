48b6b73ff0a4e7f60f030cb6375fbfc98d05ee73
MrSooo <51949526+MrSooo@users.noreply.github.com>
2020-08-13 14:42:54 +0700

    Update main.py
    +Raw movement animation of pacman and ghost

--- main.py
+++ main.py
@@ -4,15 +4,14 @@ import os
 import numpy as np
 import random
 import time
-
 pygame.init()
 
 
 # Import a maze from file name
 # Return (n, m, 2D_array_maze, pacman_i, pacman_j)
 def import_maze(filename):
-    if not os.path.exists(filename):
-        return 0, 0, None, 0, 0
+    if (not os.path.exists(filename)):
+        return (0, 0, None)
 
     file = open(filename, 'r')
 
@@ -31,10 +30,8 @@ def import_maze(filename):
 
     file.close()
 
-    return n, m, maze, pacman_i, pacman_j
+    return (n, m, maze, pacman_i, pacman_j)
 
-
-# Load map
 
 (n, m, maze, pacman_i, pacman_j) = import_maze('Maps\\Level_2\\Map_2_1.txt')
 
@@ -42,7 +39,7 @@ def import_maze(filename):
 
 screen = pygame.display.set_mode((len(maze[0]) * 20, len(maze) * 20))
 
-# change title and logo
+# change tilte and logo
 
 pygame.display.set_caption("Pacman")
 logo = pygame.image.load('pacmanicon.png')
@@ -149,30 +146,76 @@ block_corner_img = pygame.image.load('wall1.png')
 #     return False
 
 
-# movement with oder
 
-# Move player
+#movement with oder
+global score
 def move_animation(command):
     global x, y, player_img, maze
     time.sleep(0.5)
-    if command == 'LEFT' and maze[y // 20][(x - 20) // 20] != 1 and maze[y // 20][(x - 20) // 20] != 3:
+    if command == 'LEFT' and maze[y // 20][(x - 20) // 20] != 1:
         x -= 20
-        maze[y // 20][x // 20] = 0
-        player_img = player_img180
-    if command == 'RIGHT' and maze[y // 20][(x + 20) // 20] != 1 and maze[y // 20][(x + 20) // 20] != 3:
+        if maze[y // 20][x // 20] == 3:
+            score += 20
+            maze[y // 20][x // 20] = 0
+            player_img = player_img180
+        elif maze[y // 20][x // 20] == 2:
+            print('game over')
+        else:
+            score -= 1
+            player_img = player_img180
+    if command == 'RIGHT' and maze[y // 20][(x + 20) // 20] != 1:
         x += 20
-        maze[y // 20][x // 20] = 0
-        player_img = player_img0
-    if command == 'UP' and maze[(y - 20) // 20][x // 20] != 1 and maze[(y - 20) // 20][x // 20] != 3:
+        if maze[y // 20][x // 20] == 3:
+            score += 20
+            maze[y // 20][x // 20] = 0
+            player_img = player_img90
+        elif maze[y // 20][x // 20] == 2:
+            print('game over')
+        else:
+            score -= 1
+            player_img = player_img0
+    if command == 'UP' and maze[(y - 20) // 20][x // 20] != 1:
         y -= 20
-        maze[y // 20][x // 20] = 0
-        player_img = player_img90
-    if command == 'DOWN' and maze[(y + 20) // 20][x // 20] != 1 and maze[(y + 20) // 20][x // 20] != 3:
+        if maze[y // 20][x // 20] == 3:
+            score += 20
+            maze[y // 20][x // 20] = 0
+            player_img = player_img90
+        elif maze[y // 20][x // 20] == 2:
+            print('game over')
+        else:
+            score -= 1
+            player_img = player_img90
+    if command == 'DOWN' and maze[(y + 20) // 20][x // 20] != 1:
         y += 20
+        if maze[y // 20][x // 20] == 3:
+            score += 20
+            maze[y // 20][x // 20] = 0
+            player_img = player_img270
+        elif maze[y // 20][x // 20] == 2:
+            print('game over')
+        else:
+            score -= 1
+            player_img = player_img270
+
+def ghost_move_animation(command):
+    global x_ghost, y_ghost, ghost_img1, maze
+    time.sleep(0.5)
+    if command == 'LEFT' and maze[y_ghost // 20][(x_ghost - 20) // 20] != 1:
         maze[y // 20][x // 20] = 0
-        player_img = player_img270
-
-
+        x_ghost -= 20
+        maze[y // 20][x // 20] = 3
+    if command == 'RIGHT' and maze[y_ghost // 20][(x_ghost + 20) // 20] != 1:
+        maze[y // 20][x // 20] = 0
+        x_ghost += 20
+        maze[y // 20][x // 20] = 3
+    if command == 'UP' and maze[(y_ghost - 20) // 20][x_ghost // 20] != 1:
+        maze[y // 20][x // 20] = 0
+        y_ghost -= 20
+        maze[y // 20][x // 20] = 3
+    if command == 'DOWN' and maze[(y_ghost + 20) // 20][x_ghost // 20] != 1:
+        maze[y // 20][x // 20] = 0
+        y_ghost += 20
+        maze[y // 20][x // 20] = 3
 # BFS for lv 1-2
 def BFS(mtx, start, goal):
     frontier = [start]
@@ -185,20 +228,16 @@ def BFS(mtx, start, goal):
         flag[node[0]][node[1]] = 1
         expanded.append(node)
 
-        if (flag[node[0] - 1][node[1]] != 1 and (node[0] - 1, node[1]) not in frontier and mtx[node[0] - 1][
-            node[1]] != 1 and mtx[node[0] - 1][node[1]] != 3):
+        if (flag[node[0] - 1][node[1]] != 1 and (node[0] - 1, node[1]) not in frontier and mtx[node[0] - 1][node[1]] != 1 and mtx[node[0] - 1][node[1]] != 3):
             parent_list.append(((node[0] - 1, node[1]), node))
             frontier.append((node[0] - 1, node[1]))
-        if (flag[node[0] + 1][node[1]] != 1 and (node[0] + 1, node[1]) not in frontier and mtx[node[0] + 1][
-            node[1]] != 1 and mtx[node[0] + 1][node[1]] != 3):
+        if (flag[node[0] + 1][node[1]] != 1 and (node[0] + 1, node[1]) not in frontier and mtx[node[0] + 1][node[1]] != 1 and mtx[node[0] + 1][node[1]] != 3):
             parent_list.append(((node[0] + 1, node[1]), node))
             frontier.append((node[0] + 1, node[1]))
-        if (flag[node[0]][node[1] - 1] != 1 and (node[0], node[1] - 1) not in frontier and mtx[node[0]][
-            node[1] - 1] != 1 and mtx[node[0]][node[1] - 1] != 3):
+        if (flag[node[0]][node[1] - 1] != 1 and (node[0], node[1] - 1) not in frontier and mtx[node[0]][node[1] - 1] != 1 and mtx[node[0]][node[1] - 1] != 3):
             parent_list.append(((node[0], node[1] - 1), node))
             frontier.append((node[0], node[1] - 1))
-        if (flag[node[0]][node[1] + 1] != 1 and (node[0], node[1] + 1) not in frontier and mtx[node[0]][
-            node[1] + 1] != 1 and mtx[node[0]][node[1] + 1] != 3):
+        if (flag[node[0]][node[1] + 1] != 1 and (node[0], node[1] + 1) not in frontier and mtx[node[0]][node[1] + 1] != 1 and mtx[node[0]][node[1] + 1] != 3):
             parent_list.append(((node[0], node[1] + 1), node))
             frontier.append((node[0], node[1] + 1))
     expanded.append(goal)
@@ -212,7 +251,7 @@ def BFS(mtx, start, goal):
     path.append(goal)
     print(path)
     movement = []
-    for i in range(0, len(path) - 1):
+    for i in range(0, len(path)-1):
         if (path[i][0] - 1, path[i][1]) == (path[i + 1][0], path[i + 1][1]):
             movement.append('UP')
         if (path[i][0] + 1, path[i][1]) == (path[i + 1][0], path[i + 1][1]):
@@ -234,7 +273,7 @@ cmd = BFS(maze, (pacman_i, pacman_j), goal)
 while running:
     screen.fill((0, 0, 0))
 
-    # Food
+
     for i in range(0, len(maze)):
         for j in range(0, len(maze[0])):
             if maze[i][j] == 2:
@@ -248,19 +287,14 @@ while running:
     #     print('?')
     #     sys.exit()
 
-    # Ghost
+
     for i in range(0, len(ghost_list)):
         generate_object(ghost_img_list[i % 5], ghost_list[i][1] * 20, ghost_list[i][0] * 20)
-
-    # Wall
     for i in range(0, len(maze)):
         for j in range(0, len(maze[0])):
             if maze[i][j] == 1:
                 generate_object(block_img, j * 20, i * 20)
-
-    # Player
     generate_object(player_img, x, y)
-
     time.sleep(0.2)
     if t == len(cmd):
         time.sleep(0.5)
@@ -269,5 +303,4 @@ while running:
     move_animation(cmd[t])
     t += 1
     pygame.display.update()
-
 
